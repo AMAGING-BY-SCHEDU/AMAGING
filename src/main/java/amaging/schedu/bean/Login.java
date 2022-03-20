a2e86300e0f5900c1d8e2b52ddcd27b04e59f506
YuiFa80 <ky171231@yahoo.co.jp>
2022-03-20 14:46:01 +0900

    ignore_test

--- src/main/java/amaging/schedu/bean/Login.java
+++
@@ -1,9 +0,0 @@
-package amaging.schedu.bean;
-import lombok.Data;
-@Data
-public class Login {
-	String email;
-	String password;
-	int userCode;
-	String adCode;
-}
--- src/main/java/amaging/schedu/bean/Waiting.java
+++
@@ -1,8 +0,0 @@
-package amaging.schedu.bean;
-import lombok.Data;
-@Data
-public class Waiting {
-	String userId;
-	String email;
-	int status;
-}
